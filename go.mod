c06aef388405e881bb5d648c27c6400331350c9e
Renaud <renaudgdumont@gmail.com>
2022-01-27 22:47:52 -0500

    docker-compose and migration files

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.16
 
 require (
 	github.com/joho/godotenv v1.4.0
+	github.com/prisma/prisma-client-go v0.13.0 // indirect
 	github.com/stretchr/testify v1.7.0 // indirect
 	github.com/vmihailenco/msgpack/v5 v5.3.5
 )
---
+++ migrations/20220128034415_init/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE "Account" (
+    "id" BIGSERIAL NOT NULL,
+    "username" TEXT NOT NULL,
+    "password" VARCHAR(255) NOT NULL,
+    "email" VARCHAR(255) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Account_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Account_username_key" ON "Account"("username");
