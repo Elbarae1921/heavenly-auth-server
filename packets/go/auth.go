bbbce627f3e4c06c633b81ead094d7baf02913ff
packet-generator <packet@generator.auto>
2022-02-04 20:36:21 +0000

    Update from https://github.com/Heavenly-Dragons/packet-generator/commit/77aa4f93bfd7779c5086aeb99892812b8aca2faf

---
+++ packets/go/auth.go
@@ -0,0 +1,23 @@
+package auth
+
+// From Client
+type LoginMessage struct {
+   Username string
+   Password string
+}
+
+type Token struct {
+   UserId    uint64
+   ExpiresAt int64
+}
+
+// From Server
+type LoginResponse struct {
+   Token     Token
+   Signature []byte
+}
+
+// From Server
+type Error struct {
+   Message string
+}
