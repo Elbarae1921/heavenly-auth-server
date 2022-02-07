86ef721d54c7f45ae667cd8c8c46ae481cd5d720
packet-generator <packet@generator.auto>
2022-02-07 20:24:27 +0000

    Update from https://github.com/Heavenly-Dragons/packet-generator/commit/e9bea313190c0e0bf889374bd97fa550a37bd464

--- packets/mappings.go
+++ packets/mappings.go
@@ -4,19 +4,19 @@ var PACKETS_INT_TO_STRING = map[uint16]string{
   101: "LoginMessage",
   102: "RegisterMessage",
   1001: "HandshakeMessage",
-  1002: "ChatMessage"
+  1002: "ChatMessage",
 }
 
 var PACKETS_STRING_TO_INT = map[string]uint16{
   "LoginMessage": 101,
   "RegisterMessage": 102,
   "HandshakeMessage": 1001,
-  "ChatMessage": 1002
+  "ChatMessage": 1002,
 }
 
 var PACKET_TO_GAME_MESSAGE = map[string]interface{}{
   "LoginMessage": LoginMessage{},
   "RegisterMessage": RegisterMessage{},
   "HandshakeMessage": HandshakeMessage{},
-  "ChatMessage": ChatMessage{}
+  "ChatMessage": ChatMessage{},
 }
